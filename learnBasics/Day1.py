540b549ba65e1bd9332a1e4899a11ea65070dad5
surajlunthi <50191829+surajlunthi@users.noreply.github.com>
2025-01-19 18:21:02 +0530

    learn indexing

--- learnBasics/Day1.py
+++ learnBasics/Day1.py
@@ -1,3 +1,16 @@
 print('suraj\'s' 'laptop .')
-# in python if you wnat to escape some thing we will backslash proceeding the char
+# in python if you want to escape some thing we will backslash proceeding the char
 print('nacin\\n' 's')
+
+
+
+name = 'suraj'
+print(name[0])
+print(name[1])
+
+
+print(name[-1])
+print(name[0:2]) #su
+
+print(name[1:4]) #ura
+print(name[1:]+'learning concate')
